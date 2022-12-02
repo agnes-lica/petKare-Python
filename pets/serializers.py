7666c1ca094c9bb4c4fb136b8eb17b2d22446b68
Agnes-lica <agnes.lica@gmail.com>
2022-12-02 14:05:41 -0400

    feat: finishing tarefa 2

---
+++ pets/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
+
+
+class PetSerializer(serializers.Serializer):
+    SEX_CHOICES = (("Male", "Male"), ("Female", "Female"))
+
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.ChoiceField(
+        choices=SEX_CHOICES, default="Not Informed")
+
+    group = GroupSerializer(read_only=True)
+    traits = TraitSerializer(many=True, read_only=True)
